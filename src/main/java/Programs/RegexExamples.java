363bcb2cf64fc48e3822aa4c641e6fa0f52aac9d
sunshine <mytrade1086@gmail.com>
2020-01-26 21:36:08 +0530

    added regex

--- src/main/java/Programs/RegexExamples.java
+++ src/main/java/Programs/RegexExamples.java
@@ -2,27 +2,18 @@ package Programs;
 
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;;
+
 public class RegexExamples {
 
-	public static void main(String[] args) {
-		  Pattern p = Pattern.compile("\\d+");
-	        Matcher m = p.matcher("This is 123 and request is 3434. Test 123");
-	        while(m.find()) {
-	            System.out.println(m.group());
-	      for(int i=1;i<100;i++) {
-	            	
-	            	
-	            	
-	            }
-	            
-	            
+	int count = 0;
 
-	}
-	        
-	        
-	        
-	        
-	        
+	public static void main(String[] args) {
+		Pattern p = Pattern.compile("\\d+");
+		Matcher m = p.matcher("This is 123 and request is 3434. Test 123");
+		while (m.find()) {
+			System.out.println(m.group());
+
+		}
 
 	}
 }
