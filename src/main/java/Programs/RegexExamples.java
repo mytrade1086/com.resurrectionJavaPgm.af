2c62da3572a8064feb0f95e127e59008f2aa3461
sunshine <mytrade1086@gmail.com>
2020-01-26 21:35:53 +0530

    Regex For Number

---
+++ src/main/java/Programs/RegexExamples.java
@@ -0,0 +1,28 @@
+package Programs;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;;
+public class RegexExamples {
+
+	public static void main(String[] args) {
+		  Pattern p = Pattern.compile("\\d+");
+	        Matcher m = p.matcher("This is 123 and request is 3434. Test 123");
+	        while(m.find()) {
+	            System.out.println(m.group());
+	      for(int i=1;i<100;i++) {
+	            	
+	            	
+	            	
+	            }
+	            
+	            
+
+	}
+	        
+	        
+	        
+	        
+	        
+
+	}
+}
