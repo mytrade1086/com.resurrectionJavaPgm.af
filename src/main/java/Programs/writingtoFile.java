4f9fb862a9e2ecde73d9f64c24bc834442165e8a
sunshine <mytrade1086@gmail.com>
2020-02-10 23:32:38 +0530

    Buffered writer

--- src/main/java/Programs/writingtoFile.java
+++ src/main/java/Programs/writingtoFile.java
@@ -1,13 +1,31 @@
 package Programs;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class writingtoFile {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		
 		
-		FileWriter
-		BufferedWriter
+		FileWriter fw=new FileWriter("H:\\eclipseWin10\\com.resurrectionJavaPgm.af\\Files\\demowrite.txt");
+		BufferedWriter bw=new BufferedWriter(fw);
+		
+		bw.write("line1");
+		bw.newLine();
+		bw.write("line2");
+		bw.newLine();
+		bw.write("line3");
+		bw.newLine();
+	
+		
+		bw.close();
+		fw.close();
+		
+		
+		
 	}
 }
